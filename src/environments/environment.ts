fb7dc8063578cbb816ada0035596164613d0b227
Melanie <74563324+Aly-git-dev@users.noreply.github.com>
2026-05-05 17:24:07 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
   production: true,
   apiUrl: 'https://api.kalypi.xyz'
-=======
-  production: false,
-  apiUrl: 'http://localhost:8080'
->>>>>>> c8cf481 (avances hasta 050526)
 };
